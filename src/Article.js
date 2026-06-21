4f615dc722e610a2825af300fefe268805c1dd29
alex <kond9.k@yandex.ru>
2026-06-21 10:30:19 +0300

    Fix

--- src/Article.js
+++ src/Article.js
@@ -1,21 +1,17 @@
 import './Article.css';
 import Card from './Card';
+import './Card.css'; // ВАЖНО: импортируем стили для карточек
 
-function Article(props)
-{
-    return(
-
-        <div>
+function Article(props) {
+    return (
+        <div className="cards-container"> {/* ДОБАВЛЯЕМ КОНТЕЙНЕР */}
             {
-            Object.keys(props.db).map
-            (
-                elem =>
-                {
-                    return(<Card persone= {props.db[elem]}/>)
-                }
-            )
-        }
+                Object.keys(props.db).map((elem, index) => {
+                    return <Card key={index} persone={props.db[elem]} />
+                })
+            }
         </div>
     )
 }
+
 export default Article;
